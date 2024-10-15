90eaca6b5354c7831ec325ab075c190744d7cfb1
Steven Lignereux <slignere@gmail.com>
2024-10-16 01:44:27 +0200

    📦 NEW: extraire la description

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ f.close()
 soup = BeautifulSoup(html_content, "html.parser")
 
 titre_h1 = soup.find("h1")
+div_centre = soup.find("div", class_="centre")
+paragraphe_description = soup.find("p", class_="description")
+
 texte_titre = titre_h1.text
+texte_paragraphe_description = paragraphe_description.text
 
 print("Titre de la page html : ", texte_titre)
+print("Paragraphe de description de la page html : ", texte_paragraphe_description)
